cec54ab25f6d98cffeaba40cd17473dad0be0e66
Rayvant Durani <rayvant310@gmail.com>
2023-12-14 11:30:26 +0530

    Added the delete Logic from Cart

--- src/main/java/com/Ecomm_Backend/Service/userService.java
+++ src/main/java/com/Ecomm_Backend/Service/userService.java
@@ -4,13 +4,16 @@ import com.Ecomm_Backend.Model.Product;
 import com.Ecomm_Backend.Model.User;
 import com.Ecomm_Backend.Repository.userRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.ResourceBundle;
+
 
 @Service
 public class userService {
@@ -75,4 +78,21 @@ public class userService {
         }
         return ResponseEntity.ok(usersList);
         }
+
+    public boolean deleteProductforUser(String userId, String productId) {
+        Optional<User> userOptional = userR.findById(userId);
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            List<Product> prodListUser = user.getProductList();
+
+            boolean isRemoved = prodListUser.removeIf(prod -> prod.getProduct_Id().equalsIgnoreCase(productId));
+
+            if (isRemoved) {
+                userR.save(user); // Save the user with the updated product list
+                return true; // Product was successfully removed
+            }
+        }
+        return false; // User not found or product not in user's list
+    }
 }
